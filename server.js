a8c4d2c99705f0a7f675501ac042af76c9252ef8
Dmitry <diskibin@gmail.com>
2021-09-28 23:25:21 +0300

    Remove node-mysql-admin

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const db = require("./app/models");
-const mysqlAdmin = require('node-mysql-admin');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -23,8 +22,6 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(mysqlAdmin(app));
 
 require("./app/routes/pvu.routes")(app);
 
