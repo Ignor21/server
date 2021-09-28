0a4c700bbfaa861d2812963d8813fb8071ee2a8e
Dmitry <diskibin@gmail.com>
2021-09-28 14:56:59 +0300

    Testing API

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const db = require("./app/models");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and re-sync db.");
+});
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'IT\'S WORKING!' });
 });
+
+require("./app/routes/turorial.routes")(app);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
