dce08a06ba606918a09877c6e2067f0515e52ea1
Dmitry <diskibin@gmail.com>
2021-09-28 15:39:42 +0300

    Fix DB #4

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const db = require("./app/models");
+const mysql = require("mysql2");
+const dbConfig = require("../config/db.config.js");
+  
+const connection = mysql.createConnection({
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  database: dbConfig.DB,
+  password: dbConfig.PASSWORD
+});
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,7 +18,7 @@ const port = process.env.PORT || 3000;
 db.sequelize.sync({ force: true }).then(() => {
   console.log("Drop and re-sync db.");
 }).catch(function (error) {
-     console.log(error);
+  console.log(error);
 });
 
 var corsOptions = {
