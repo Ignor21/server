15e06264b6a12da82f1992939787893a7dde72d4
Dmitry <diskibin@gmail.com>
2021-09-28 23:04:44 +0300

    node-mysql-admin

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const db = require("./app/models");
+const mysqlAdmin = require('node-mysql-admin');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -14,6 +15,7 @@ db.sequelize.sync({ force: true }).then(() => {
 
 var corsOptions = {
   origin: "*"
+  // origin: "http://localhost:8081" - to enable CORS
 };
 
 app.use(cors(corsOptions));
@@ -21,6 +23,8 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(mysqlAdmin(app));
 
 require("./app/routes/pvu.routes")(app);
 
