10dcbac22652317ec46ca22892306240d65ded81
Dmitry <diskibin@gmail.com>
2021-10-01 16:17:14 +0300

    Marketplace DB addition

--- app/functions/marketplace.functions.js
+++ app/functions/marketplace.functions.js
@@ -5,7 +5,7 @@ const https = require('https');
 exports.getMarketplaceFromRemote = () => {
   let options = {
     host: 'backend-farm.plantvsundead.com',
-    path: '/get-plants-filter-v2?offset=0&limit=10&type=1',
+    path: '/get-plants-filter-v2?offset=0&limit=1000000&type=1',
     method: 'GET',
     headers: {
       'authorization': 'Bearer Token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwdWJsaWNBZGRyZXNzIjoiMHgzMTU4ZTA0ZTE4MjRiZWZjYjhjNGQ4NWY4Mjk0MmM2OTJmYjNmYWMyIiwibG9naW5UaW1lIjoxNjMyODYyNTc0MTI0LCJjcmVhdGVEYXRlIjoiMjAyMS0wNy0xOSAwNzoyMzozNCIsImlhdCI6MTYzMjg2MjU3NH0.W7r6UqBo_NZaCqnP7L9xlIR22xjCH_2qtD1uZiAfR1c',
