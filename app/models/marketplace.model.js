a125330f03c611121c916c12dcc7f3bfa4f34034
Dmitry <diskibin@gmail.com>
2021-10-01 16:25:13 +0300

    Update marketplace.model.js

--- app/models/marketplace.model.js
+++ app/models/marketplace.model.js
@@ -1,7 +1,7 @@
 module.exports = (sequelize, Sequelize) => {
   const Marketplace = sequelize.define("marketplace", {
     id: {
-      type: Sequelize.INTEGER,
+      type: Sequelize.STRING,
       primaryKey: true
     },
     config: {
